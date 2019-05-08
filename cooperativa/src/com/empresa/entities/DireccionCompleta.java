b52e824c310ee40b8a069a9f37403cebb3d8b182
Marco Castro <marcovcg1819@gmail.com>
2019-05-07 21:21:15 -0600

    modelo de direccion completa

---
+++ cooperativa/src/com/empresa/entities/DireccionCompleta.java
@@ -0,0 +1,37 @@
+package com.empresa.entities;
+
+public class DireccionCompleta {
+	private String calle;
+	private String barrio;
+	private String numeroCasa;
+	private String municipio;
+	public DireccionCompleta() {
+		super();
+	}
+	public String getCalle() {
+		return calle;
+	}
+	public void setCalle(String calle) {
+		this.calle = calle;
+	}
+	public String getBarrio() {
+		return barrio;
+	}
+	public void setBarrio(String barrio) {
+		this.barrio = barrio;
+	}
+	public String getNumeroCasa() {
+		return numeroCasa;
+	}
+	public void setNumeroCasa(String numeroCasa) {
+		this.numeroCasa = numeroCasa;
+	}
+	public String getMunicipio() {
+		return municipio;
+	}
+	public void setMunicipio(String municipio) {
+		this.municipio = municipio;
+	}
+	
+	
+}
